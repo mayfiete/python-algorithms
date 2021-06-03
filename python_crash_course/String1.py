3fa7f46a36ca1afa7ac5dc0a8378158c2335e098
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-06-02 21:43:35 -0400

    feat: update String1.py

--- python_crash_course/String1.py
+++ python_crash_course/String1.py
@@ -53,3 +53,8 @@ print("-".join(split_message))
 
 
 print(string.ascii_lowercase) #import all lowercase letters
+
+stock_price = "1100"  
+print("Today's price for google stock is: {}".format(stock_price))
+
+print(f"Today's price for google stock is: {stock_price}")
